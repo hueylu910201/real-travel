99f3fb68fba41a9a2aa6982befe8964bac069312
hueylu910201 <hueylu910201@gmail.com>
2023-08-05 01:52:33 +0800

    data to json

--- src/App.jsx
+++ src/App.jsx
@@ -1,47 +1,20 @@
 import React from 'react';
-import { useState, useEffect, useRef, useLayoutEffect } from 'react'
-import { MapContainer, TileLayer, useMap, Marker, Popup, Polygon, WMSTileLayer } from 'react-leaflet'
+import { useState, useRef } from 'react'
+import { MapContainer, TileLayer, Marker, Popup, Polygon, WMSTileLayer } from 'react-leaflet'
 import 'leaflet/dist/leaflet.css'
+import SearchBar from './SearchBar';
 import './App.css'
-import styles from "./styles.module.css"
 import L from 'leaflet';
-import SearchIcon from '@mui/icons-material/Search';
+import data from "./Data.json"
 
 
 function App() {
 
+
   //多邊形範圍
   const purpleOptions = { color: 'purple' };
-  const polygon = [
-    [25.024973524288985, 121.54413342481826],
-    [25.024831345073277, 121.54643207789951],
-    [25.02285732931282, 121.54752050047698],
-    [25.0220289657309, 121.54320382037442],
-  ]
+  const polygon = data.map(marker => marker.position);
 
-  //JSON資料
-  const markerPos = [
-    {
-      name: '烏弄',
-      position: [25.024973524288985, 121.54413342481826],
-      img: 'public/烏弄.jpg',
-    },
-    {
-      name: '可不可熟成紅茶',
-      position: [25.024831345073277, 121.54643207789951],
-      img: 'public/可不可.jpg',
-    },
-    {
-      name: '不知道',
-      position: [25.02285732931282, 121.54752050047698],
-      img: 'public/不知道.jpg',
-    },
-    {
-      name: '米堤銀行',
-      position: [25.0220289657309, 121.54320382037442],
-      img: 'public/米堤.jpg',
-    },
-  ]
 
   //點擊圖片使該marker置中
   const mapRef = useRef(null);
@@ -66,52 +39,18 @@ function App() {
     opacity: 1,
   };
 
-  //搜尋filter
-  const [value, setValue] = useState('');
-  const [filterData, setFilterData] = useState([]);
-  const onChange = (event) => {
-    const searchItem = event.target.value;
-    setValue(searchItem);
-    if (searchItem === '') {
-      setFilterData([]); // Clear the suggestions
-    } else {
-      const newFilter = markerPos.filter((item) => {
-        return item.name.toLowerCase().includes(searchItem.toLowerCase());
-      }).slice(0,10);
-      setFilterData(newFilter);
-    }
-  }
 
-  const onSearch = (searchItem) => {
-    setValue(searchItem);
-    console.log("內容:", searchItem);
-    const marker = markerPos.find(item => item.name.toLowerCase() === searchItem.toLowerCase());
+  const onSearchResult = (searchItem) => {
+    const marker = data.find(item => item.name.toLowerCase() === searchItem.toLowerCase());
     if (marker && mapRef.current) {
       const map = mapRef.current;
       map.flyTo(marker.position, 16);
     }
-    setFilterData([]);
   }
 
   return (
     <div>
-      <div className={styles.searchBar}>
-        <div className={styles.searchInput}>
-          <input id="searchbar" type="text" placeholder='尋找店家' value={value} onChange={onChange}
-            style={{ width: '30vw', height: '4vh' ,borderStyle:'solid',borderRight:'none'}} />
-          <button className={styles.searchButton}onClick={() => onSearch(value)}>搜尋</button>
-        </div>
-        <div style={{ display: 'flex', justifyContent: 'center' }}>
-          {filterData.length !== 0 && (
-            <div className={styles.suggestion}>
-              {filterData.map((item, index) => (
-                <div onClick={() => { onSearch(item.name) }} key={index} style={{ display: 'flex' }}>{item.name}</div>
-              ))}
-            </div>
-          )}
-        </div>
-
-      </div>
+      <SearchBar placeholder="尋找店家" data={data} onSearchResult={onSearchResult}/>
       <div style={{ marginTop: '3rem' }}>
         <MapContainer center={[25.02308934789089, 121.54513940000001]} zoom={16} scrollWheelZoom={true} ref={mapRef} style={{ height: '60vh', width: '50vw' }}>
           <TileLayer
@@ -125,7 +64,7 @@ function App() {
             </Popup>
           </Marker>
 
-          {markerPos.map((marker, index) => (
+          {data.map((marker, index) => (
             <Marker key={index} position={marker.position} >
               <Popup>
                 <div>
@@ -140,7 +79,7 @@ function App() {
         </MapContainer>
       </div>
       <div style={{ width: '50vw', height: '25vh', display: 'flex', flexDirection: 'row', justifyContent: 'space-around' }}>
-        {markerPos.map((marker, index) => (
+        {data.map((marker, index) => (
           <div key={index} style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
             <img src={marker.img} alt={marker.name} style={{ width: '10rem' }} onClick={() => markerClick(marker)} />
             <a>{marker.name}</a>
--- src/SearchBar.jsx
+++ src/SearchBar.jsx
@@ -1,69 +1,55 @@
 import React from 'react';
+import styles from "./styles.module.css"
 import { useState, useRef } from 'react'
 import 'leaflet/dist/leaflet.css'
 
-export default function SearchBar() {
-  const markerPos = [
-    {
-      name: '烏弄',
-      position: [25.024973524288985, 121.54413342481826],
-      img: 'public/烏弄.jpg',
-    },
-    {
-      name: '可不可熟成紅茶',
-      position: [25.024831345073277, 121.54643207789951],
-      img: 'public/可不可.jpg',
-    },
-    {
-      name: '不知道',
-      position: [25.02285732931282, 121.54752050047698],
-      img: 'public/不知道.jpg',
-    },
-    {
-      name: '米堤銀行',
-      position: [25.0220289657309, 121.54320382037442],
-      img: 'public/米堤.jpg',
-    },
-  ]
-
+export default function SearchBar({ placeholder, data ,onSearchResult}) {
+  const mapRef = useRef();
 
   //搜尋filter
-  const mapRef = useRef(null);
   const [value, setValue] = useState('');
+  const [filterData, setFilterData] = useState([]);
   const onChange = (event) => {
-    setValue(event.target.value);
+    const searchItem = event.target.value;
+    setValue(searchItem);
+    if (searchItem === '') {
+      setFilterData([]); // Clear the suggestions
+    } else {
+      const newFilter = data.filter((item) => {
+        return item.name.toLowerCase().includes(searchItem.toLowerCase());
+      }).slice(0, 10);
+      setFilterData(newFilter);
+    }
   }
+
   const onSearch = (searchItem) => {
     setValue(searchItem);
     console.log("內容:", searchItem);
-    const marker = markerPos.find((item) => item.name.toLowerCase() === searchItem.toLowerCase());
-    if (marker && mapRef.current) {
-      const map = mapRef.current;
-      map.flyTo(marker.position, 16);
-    }
+    const marker = data.find(item => item.name.toLowerCase() === searchItem.toLowerCase());
+    // if (marker && mapRef.current) {
+    //   const map = mapRef.current;
+    //   map.flyTo(marker.position, 16);
+    // }
+    onSearchResult(searchItem);
+    setFilterData([]);
   }
   return (
-    <div style={{ width: '50wh', display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
-      <div style={{ marginLeft: '4.5rem' }}>
-        <input id="searchbar" type="text" placeholder='尋找店家' value={value} onChange={onChange}
-          style={{ width: '30vw', height: '4vh' }} />
-        <button onClick={() => onSearch(value)}>搜尋</button>
+    <div className={styles.searchBar}>
+      <div className={styles.searchInput}>
+        <input id="searchbar" type="text" placeholder={placeholder} value={value} onChange={onChange}
+          style={{ width: '30vw', height: '4vh', borderStyle: 'solid', borderRight: 'none' }} />
+        <button className={styles.searchButton} onClick={() => onSearch(value)}>搜尋</button>
+      </div>
+      <div style={{ display: 'flex', justifyContent: 'center' }}>
+        {filterData.length !== 0 && (
+          <div className={styles.suggestion}>
+            {filterData.map((item, index) => (
+              <div onClick={() => { onSearch(item.name) }} key={index} style={{ display: 'flex' }}>{item.name}</div>
+            ))}
+          </div>
+        )}
       </div>
 
-      <div style={{
-        display: 'flex', flexDirection: 'column', borderStyle: 'solid',
-        borderTopStyle: 'none', borderBottomStyle: 'none', borderColor: 'black', borderWidth: '1px',
-        width: '30vw', height: '10vh',cursor:'pointer'
-      }}>
-        {markerPos.filter(item => {
-          const searchItem = value.toLowerCase();
-          const name = item.name.toLowerCase();
-          return searchItem && name.startsWith(searchItem) && name !== searchItem
-        }).slice(0, 10)
-          .map((item, index) => (
-            <div onClick={() => { onSearch(item.name) }} key={index} style={{ display: 'flex' }}>{item.name}</div>
-          ))}
-      </div>
     </div>
   );
 }
